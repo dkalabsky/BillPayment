4995a00d0c820ea5733a685ae6cd37815b9ae6db
dkalabsky <dkalabsky@gmail.com>
2023-05-23 18:43:13 +0300

    Add default structure

---
+++ service/src/main/java/ru/home/BillPayment.java
@@ -0,0 +1,12 @@
+package ru.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillPayment {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillPayment.class, args);
+    }
+}
